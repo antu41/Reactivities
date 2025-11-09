34bd16b37dec8d6432c272f40795c1283a5e9127
Antu <antudas444475@email.com>
2025-11-09 16:24:46 +0600

    Implemented Server side Validation and Error Handling

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,5 @@
 using Application.Activities.Commands;
+using Application.Activities.DTOs;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
@@ -8,7 +9,7 @@ namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    [HttpGet]
+
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
         return await Mediator.Send(new GetActivityList.Query());
@@ -17,29 +18,29 @@ public class ActivitiesController : BaseApiController
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        return HandleResult(await Mediator.Send(new GetActivityDetails.Query { Id = id }));
+
+
     }
 
     [HttpPost]
-    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    public async Task<ActionResult<string>> CreateActivity(CreateActivityDto activityDto)
     {
-        return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+        return HandleResult(await Mediator.Send(new CreateActivity.Command { ActivityDto = activityDto }));
     }
 
     [HttpPut]
-    public async Task<ActionResult<string>> EditActivity(Activity activity)
+    public async Task<ActionResult<string>> EditActivity(EditActivityDto activity)
     {
-        await Mediator.Send(new EditActivity.Command { Activity = activity });
+        return HandleResult(await Mediator.Send(new EditActivity.Command { ActivityDto = activity }));
 
-        return NoContent();
     }
 
     [HttpDelete("{id}")]
     public async Task<ActionResult<string>> DeleteActivity(string id)
     {
-        await Mediator.Send(new DeleteActivity.Command { Id = id });
+        return HandleResult(await Mediator.Send(new DeleteActivity.Command { Id = id }));
 
-        return Ok();
     }
 
 }
--- Application/Activities/Commands/CreateActivity.cs
+++ Application/Activities/Commands/CreateActivity.cs
@@ -1,5 +1,9 @@
 using System;
+using Application.Activities.DTOs;
+using Application.Core;
+using AutoMapper;
 using Domain;
+using FluentValidation;
 using MediatR;
 using Persistence;
 
@@ -7,22 +11,27 @@ namespace Application.Activities.Commands;
 
 public class CreateActivity
 {
-    public class Command : IRequest<string>
+    public class Command : IRequest<Result<string>>
     {
-        public required Activity Activity { get; set; }
+        public required CreateActivityDto ActivityDto { get; set; }
     }
 
-    public class Handler(AppDbContext context) : IRequestHandler<Command, string>
+    public class Handler(AppDbContext context, IMapper mapper) : IRequestHandler<Command, Result<string>>
     {
-        public async Task<string> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<Result<string>> Handle(Command request, CancellationToken cancellationToken)
         {
-            request.Activity.Date = DateTime.SpecifyKind(request.Activity.Date, DateTimeKind.Utc);
 
-            context.Activities.Add(request.Activity);
+            var activity = mapper.Map<Activity>(request.ActivityDto);
 
-            await context.SaveChangesAsync(cancellationToken);
+            activity.Date = DateTime.SpecifyKind(activity.Date, DateTimeKind.Utc);
 
-            return request.Activity.Id;
+            context.Activities.Add(activity);
+
+            var result = await context.SaveChangesAsync(cancellationToken) > 0;
+
+            if (!result) return Result<string>.Failure("Failed to create the activity", 400);
+
+            return Result<string>.Success(activity.Id);
         }
     }
 
