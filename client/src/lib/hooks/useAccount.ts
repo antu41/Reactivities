26326cb49aa5c0acc31001bde5c1d49cc02d73e8
Antu <antudas444475@email.com>
2026-02-13 22:21:44 +0600

    Social login complete

--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -89,6 +89,18 @@ export const useAccount = () => {
     },
   });
 
+  const fetchGithubToken = useMutation({
+    mutationFn: async (code: string) => {
+      const response = await agent.post(`/account/github-login?code=${code}`);
+      return response.data;
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+    },
+  });
+
   return {
     loginUser,
     currentUser,
@@ -100,5 +112,6 @@ export const useAccount = () => {
     changePassword,
     forgotPassword,
     resetPassword,
+    fetchGithubToken,
   };
 };
